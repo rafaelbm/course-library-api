f36c041e25a43bb3d1f6364a5b4e1f25569102ad
Rafael B Machado <rafaeobm@hotmail.comm>
2020-01-30 19:26:46 -0300

    Implement CreateAuthor

--- CourseLibrary.API/Profiles/AuthorsProfile.cs
+++ CourseLibrary.API/Profiles/AuthorsProfile.cs
@@ -15,6 +15,8 @@ namespace CourseLibrary.API.Profiles
                     dest => dest.Age,
                     opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge())
                 );
+
+            CreateMap<Models.AuthorForCreationDto, Entities.Author>();
         }
     }
 }
