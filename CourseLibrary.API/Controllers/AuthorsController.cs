cf21d38d6636ab7b8e5958bc5de5c962f57e6b77
Rafael B Machado <rafaeobm@hotmail.comm>
2020-01-29 22:47:52 -0300

    Add search capabilities

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
+using CourseLibrary.API.ResourcesParameters;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -26,9 +27,9 @@ namespace CourseLibrary.API.Controllers
         [HttpGet]
         [HttpHead]
         public ActionResult<IEnumerable<AuthorDto>> GetAuthors(
-            string mainCategory)
+            [FromQuery] AuthorsResourceParameters authorsResourceParameters)
         {
-            var authorsFromRepo = _courseLibraryRepository.GetAuthors(mainCategory);
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
             return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
         }
 
