f28067e86a129b62ee9ee5fd44757ae965028d19
Rafael B Machado <rafaeobm@hotmail.comm>
2020-01-30 20:34:03 -0300

    Add Validation for CourseForCreationDto

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different from description.")]
+    public class CourseForCreationDto //: IValidatableObject
     {
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
+
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should sbe different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
