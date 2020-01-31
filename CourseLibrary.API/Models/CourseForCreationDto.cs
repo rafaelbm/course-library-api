9d4231198bd3e97d6e9089c29c2388109df84fba
Rafael B Machado <rafaeobm@hotmail.comm>
2020-01-30 21:03:43 -0300

    Refactor Dtos to implement a common abstract class

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -3,24 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    [CourseTitleMustBeDifferentFromDescription(ErrorMessage = "Title must be different from description.")]
-    public class CourseForCreationDto //: IValidatableObject
+    public class CourseForCreationDto : CourseForManipulationDto
     {
-        [Required(ErrorMessage = "You should fill out a title.")]
-        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
-        public string Title { get; set; }
-
-        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
-        public string Description { get; set; }
-
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (Title == Description)
-        //    {
-        //        yield return new ValidationResult(
-        //            "The provided description should sbe different from the title.",
-        //            new[] { "CourseForCreationDto" });
-        //    }
-        //}
     }
 }
